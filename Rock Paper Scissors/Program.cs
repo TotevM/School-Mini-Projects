3a38a9b16b2a72fe4584cfbcaaa81f51591de386
TotevM <mtotev53@gmail.com>
2024-02-02 22:35:32 +0200

    Rewrote an exercise

---
+++ Rock Paper Scissors/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RockPaperScissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Chose between rock, paper or scissors by typing it here: ");
+            List<string> possibleMoves= new List<string>() { "rock","paper","scissors"};
+            string playerMove = Console.ReadLine().ToLower();
+            
+
+            if (!possibleMoves.Contains(playerMove))
+            {
+                Console.WriteLine("Try again with a correct input!");
+                return;
+            }
+
+            Random random = new Random();
+            int computerRandomNumber = random.Next(1, 4);
+            string computerMove = possibleMoves[computerRandomNumber];
+
+
+            Console.WriteLine($"The computer chose {computerMove}.");
+
+            if ((playerMove == "rock" && computerMove == "scissors") || (playerMove == "paper" && computerMove == "rock")
+                || (playerMove == "scissors" && computerMove ==  "paper"))
+            {
+                Console.WriteLine("You won!");
+            }
+            else if (playerMove==computerMove)
+            {
+                Console.WriteLine("Draw!");
+            }
+            else
+            {
+                Console.WriteLine("You lost!");
+            }
+
+        }
+    }
+}
